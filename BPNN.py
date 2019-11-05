376c48bbbc33c23a5a47b0068e5487d97bf7017e
Senhong Liu <amazingliux@gmail.com>
2019-11-05 14:20:40 -0500

    Update BPNN.py

--- BPNN.py
+++ BPNN.py
@@ -1,6 +1,12 @@
+"""
+This file conduct the Behler–Parinello Neural Network (BPNN). The network is conducted according to the paper 
+"Constructing High-Dimensional Neural Network Potentials: A Tutorial Review" 
+(https://onlinelibrary.wiley.com/doi/full/10.1002/qua.24890).
+"""
+
+
 import autograd.numpy as np
-from autograd import jacobian
-from autograd import elementwise_grad
+from autograd import jacobian 
 from nn import *
 from Gaussian import *
 
@@ -13,16 +19,21 @@ class BPNN:
         # generate NNs for every element
         self.NN = []
         self.sizes = params['sizes']
-        # insert the input layer
+        
+        # insert the input layer, since for radial symmetry function, the number of pair terms would
+        # be equal to the number of elements in the configuration. For example, for Au-Pd system, the pair
+        # terms would be Au-Au and Au-Pd when the center atom is Au. 
+        # For angular symmetry function, the number of triplet terms would be a permulation problem.
         self.sizes.insert(0, n_radial * len(self.elements) + n_angular * np.sum(np.arange(len(self.elements) + 1)))
-        # insert the output layer
+        # insert the output layer, it will always be one for single neural network.
         self.sizes.append(1)
+        
+        # initialize neuralnetwork for different elements
         for i in range(len(self.elements)):
             self.NN.append(neuralnetwork(self.sizes))
         
-        # generate fingerprints
+        # generate fingerprints and the derivative of fingerprints w.r.t. atomic position
         self.fp_params = params['fp_params']
-        self.mode = mode
         self.n_radial = n_radial 
         self.n_angular = n_angular
         self.fps, self.dfpdX = self.generate_fps()
@@ -31,28 +42,49 @@ class BPNN:
         self.F_coeff = params['F_coeff']
 
     def data(self):
-        E = []
-        F = []
-        atoms = []
+        """
+        Extract data from database
+        -----------
+        Returns:
+        ------------
+        E, F: np.ndarray
+            The value of energy and forces for the configuration. 
+        atoms_set: list
+            A list of configurations in the database.
+        numbers: np.ndarray
+            A matrix that contains the atomic number in all of the configurations.
+        num_atoms: int
+            A scalar value that is the number of atoms in the configurations.
+        """
+        
+        E_labels = []
+        F_labels = []
+        atoms_set = []
         numbers = []
+        
         for d in self.db.select():
             E.append(d.energy)
             F.append(d.forces)
             atoms.append(d.toatoms())
             numbers.append(d.toatoms().numbers)
+            
         num_atoms = len(atoms[0].positions)
-        return np.array(E), np.array(F), atoms, np.array(numbers), num_atoms
-
+        return np.array(E), np.array(F), atoms_set, np.array(numbers), num_atoms
 
     def generate_fps(self):
+        """
+        Generate not only the fingerprints of all the configuration, but also the derivative of 
+        fingerprints w.r.t. atoms position. The process of derivation is conducted by the autograd.
+        """
+        
         fps = []
         dfpdX = []
         for atoms in self.atoms_set:
-            single_fps = []
+            atoms_fps = []
             single_dfpdX = None
             for idx in range(self.n_radial):
                 g2 = G2(atoms.positions, atoms.cell, atoms.numbers, self.elements, self.fp_params['radial'][idx])
-                single_fps.extend(g2.T)
+                atoms_fps.extend(g2.T)
 
                 try:
                     single_dfpdX = np.concatenate((single_dfpdX, self.dG2dR(atoms.positions, atoms.cell, atoms.numbers, self.elements, self.fp_params['radial'][idx])), axis = 1)
@@ -60,10 +92,16 @@ class BPNN:
                     single_dfpdX = self.dG2dR(atoms.positions, atoms.cell, atoms.numbers, self.elements, self.fp_params['radial'][idx])
 
             for idx in range(self.n_angular):
-                g4 = G4(atoms.positions, atoms.cell, self.fp_params['angular'][idx])
-                single_fps.append(g4)
-                single_dfpdX.append(self.dG4dR(atoms.positions, atoms.cell, self.fp_params['angular'][idx]))
-            fps.append(np.array(single_fps).T)
+                g4 = G4(atoms.positions, atoms.cell, atoms.numbers, self.elements, self.fp_params['angular'][idx])
+                atoms_fps.extend(g4.T)
+                
+                try:
+                    single_dfpdX = np.concatenate((single_dfpdX, self.dG4dR(atoms.positions, atoms.cell, atoms.numbers, self.elements, self.fp_params['angular'][idx])), axis = 1)
+                except:
+                    single_dfpdX = self.dG4dR(atoms.positions, atoms.cell, atoms.numbers, self.elements, self.fp_params['angular'][idx])
+
+
+            fps.append(np.array(atoms_fps).T)
             dfpdX.append(np.array(single_dfpdX))
 
         return np.array(fps), np.array(dfpdX)
@@ -77,22 +115,41 @@ class BPNN:
         dGdR = jacobian(G4, argnum = 0)
         return dGdR(positions, cell, fp_params)
 
+    
+    
     def predict_value(self, weights):
+        """
+        Computing the energy and forces of the configurations.
+        Parameters:
+        -----------
+        weights: np.ndarray
+            It would a huge array contains all of the weights in the neuralnetwork.
+            The weights matrix should be an attribute of the neuralnetwork. In the following part,
+            the netwrok would be trained by Scipy.optimize.minimize to optimize the weights.
+            
+        Returns:
+        ------------
+        Predict value of energy and forces.
+        """
+        
         E_predict = []
         dEdfp = []
         F_predict = np.zeros(np.array(self.F).shape)
+        
+        # create a mask for different elements
+        mask = []
+        for element in self.elements:
+            mask.append((list(map(lambda x: x == element, self.numbers)) * np.ones(self.numbers.shape)).T)
+        mask = np.array(mask).T
+        
+        # loop over every single element to get the total energy and force
         for idx, element in enumerate(self.elements):
-            mask = list(map(lambda x: x == element, self.numbers)) * np.ones(self.numbers.shape)
             nn = self.NN[idx]
             weight = weights[idx * len(nn.weights): (idx + 1) * len(nn.weights)] 
             nn.weights = weight
             for j in range(len(self.atoms_set)):
                 fps = self.fps[j]
-                
                 E_predict.append(np.sum(nn.feedforward(weight, fps, mask[j][:, None])))
-                # print(elementwise_grad(nn.feedforward, argnum = 1)(weight, fps, mask[j][:, None]))
-                # print(nn.dEdfp(weight, mask[j][:, None]))
-                # dEdfp.append(elementwise_grad(nn.feedforward, argnum = 1)(weight, fps, mask[j][:, None]))
                 dEdfp.append(nn.dEdfp(weight, mask[j][:, None])) 
         
         E_predict = np.array(E_predict)
